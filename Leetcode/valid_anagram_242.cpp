01e1ba7711343e2b31cf6fb0f52857f07b27c1d9
KeplerInCoding <shreyakv8@gmail.com>
2023-08-09 00:10:17 +0530

    .

--- Leetcode/valid_anagram_242.cpp
+++ Leetcode/valid_anagram_242.cpp
@@ -1,12 +1,35 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.length()!=t.length()) return false;
+//         for(int i =0; i< t.length(); i++){
+//             int index = s.find(t[i]);
+//             if(index== string::npos) return false;
+//             s.erase(index,1);
+//         }
+//         return true;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
-        for(int i =0; i< t.length(); i++){
-            int index = s.find(t[i]);
-            if(index== string::npos) return false;
-            s.erase(index,1);
+        int table[256]={0};
+
+        for(int i=0; i<s.size(); i++){
+            table[s[i]]++;
         }
+
+        for(int i=0; i<t.size(); i++){
+            table[t[i]]--;
+        }
+
+        for(int i=0; i<256; i++){
+            if(table[i]!=0)return false;
+        }
+
         return true;
+
     }
 };
