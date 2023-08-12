917d097fce0101d91d60ed55fe31be80ec8f963e
KeplerInCoding <shreyakv8@gmail.com>
2023-08-13 00:00:07 +0530

    .

---
+++ Leetcode/isomorphicStrings_205.cpp
@@ -0,0 +1,27 @@
+//easy
+class Solution {
+    #include <unordered_map>
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map <char, char> h;
+        set <char> mySet;
+        int m = s.size();
+        int n = t.size();
+        if(m!=n) return false;
+
+        for(int i=0; i<m; i++){
+            if(h.find(s[i])==h.end()){
+                if(mySet.count(t[i])!=1){
+                    h[s[i]]=t[i];
+                    mySet.insert(t[i]);
+                }
+                else return false;
+                
+            }
+            
+            else if(h[s[i]]!=t[i]) return false;
+
+        }
+        return true;
+    }
+};
