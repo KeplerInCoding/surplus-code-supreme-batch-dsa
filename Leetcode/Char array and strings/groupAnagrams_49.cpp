8ef0f76199f286a6802178c80aac28c0b208e13e
KeplerInCoding <shreyakv8@gmail.com>
2023-08-15 00:56:33 +0530

    .

--- Leetcode/Char array and strings/groupAnagrams_49.cpp
+++ Leetcode/Char array and strings/groupAnagrams_49.cpp
@@ -37,3 +37,24 @@
 //         return V;
 //     }
 // };
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        map <string, vector<string>> mp;
+
+        for(auto s:strs){
+            string str = s;
+            sort(str.begin(), str.end());
+            mp[str].push_back(s);
+        }
+ 
+        vector <vector<string>> ans;
+
+        for(auto it=mp.begin();it!=mp.end(); it++){
+            ans.push_back(it->second);
+        }
+        return ans;
+    }
+};
