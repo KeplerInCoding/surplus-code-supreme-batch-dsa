487801d7b9892a791c892c224bea3731602ac814
KeplerInCoding <shreyakv8@gmail.com>
2024-01-01 15:44:11 +0530

    1

---
+++ Leetcode/Tree/pathSum3_437.cpp
@@ -0,0 +1,34 @@
+//medium
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int ans = 0;
+
+    void sumFromRoot(TreeNode* root, long long sum){
+        if(!root) return;
+
+        if(sum==root->val) ans++;
+
+        sumFromRoot(root->left, sum-root->val);
+        sumFromRoot(root->right, sum-root->val);
+    }
+    int pathSum(TreeNode* root, long long targetSum) {
+        if(!root) return ans;
+        sumFromRoot(root, targetSum);
+        pathSum(root->left, targetSum);
+        pathSum(root->right, targetSum);
+        return ans;
+    }
+};
